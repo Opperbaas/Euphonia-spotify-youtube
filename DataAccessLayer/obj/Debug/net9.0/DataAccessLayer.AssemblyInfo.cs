48dae06188910a17713dce3fb10ebb2700c8a951
Subiibabii1oak <152098083+Subiibabii1oak@users.noreply.github.com>
2025-11-19 12:12:53 +0100

    Update build outputs for .NET SDK 9.0.307
    Regenerated binaries and build artifacts for BusinessLogicLayer, DataAccessLayer, and PresentationLayer to use .NET SDK version 9.0.307. This includes updated assembly info, cache files, and runtime identifier graph paths.

--- DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a70346704988fb1ebb95327764f5144363a4f27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a54d5098e76e17ebf3430825352df89362d7bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
