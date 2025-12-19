5b16e6b3587c4af3f0956581cbab6b4bb83391cf
Subiibabii1oak <152098083+Subiibabii1oak@users.noreply.github.com>
2025-12-19 14:36:48 +0100

    migration script AI

---
+++ migration_script.sql
@@ -0,0 +1,68 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251104124304_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [SpecificEntities] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(100) NOT NULL,
+        [Description] nvarchar(500) NOT NULL,
+        [IsActive] bit NOT NULL DEFAULT CAST(1 AS bit),
+        [CreatedDate] datetime2 NOT NULL DEFAULT (GETDATE()),
+        [ModifiedDate] datetime2 NULL,
+        CONSTRAINT [PK_SpecificEntities] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251104124304_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_SpecificEntities_Name] ON [SpecificEntities] ([Name]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251104124304_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251104124304_InitialCreate', N'8.0.0');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251219130816_AddYouTubeFields'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251219130816_AddYouTubeFields', N'8.0.0');
+END;
+GO
+
+COMMIT;
+GO
+
